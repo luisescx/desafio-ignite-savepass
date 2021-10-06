32c5b21b0f21eee09ae04838a13a80380e0bd81f
luisescx <luis.esculapio@live.com>
2021-10-05 22:03:18 -0300

    entraga do desafio

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,91 +1,121 @@
-import React, { useState, useCallback } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useFocusEffect } from '@react-navigation/native';
+import React, { useState, useCallback } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 
-import { Header } from '../../components/Header';
-import { SearchBar } from '../../components/SearchBar';
-import { LoginDataItem } from '../../components/LoginDataItem';
+import { Header } from "../../components/Header";
+import { SearchBar } from "../../components/SearchBar";
+import { LoginDataItem } from "../../components/LoginDataItem";
 
 import {
-  Container,
-  Metadata,
-  Title,
-  TotalPassCount,
-  LoginList,
-} from './styles';
+    Container,
+    Metadata,
+    Title,
+    TotalPassCount,
+    LoginList,
+} from "./styles";
 
 interface LoginDataProps {
-  id: string;
-  service_name: string;
-  email: string;
-  password: string;
+    id: string;
+    service_name: string;
+    email: string;
+    password: string;
 }
 
 type LoginListDataProps = LoginDataProps[];
 
 export function Home() {
-  const [searchText, setSearchText] = useState('');
-  const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
-  const [data, setData] = useState<LoginListDataProps>([]);
+    const [searchText, setSearchText] = useState("");
+    const [searchListData, setSearchListData] = useState<LoginListDataProps>(
+        []
+    );
+    const [data, setData] = useState<LoginListDataProps>([]);
 
-  async function loadData() {
-    const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
-  }
+    async function loadData() {
+        const dataKey = "@savepass:logins";
+        // Get asyncStorage data, use setSearchListData and setData
 
-  function handleFilterLoginData() {
-    // Filter results inside data, save with setSearchListData
-  }
+        const storagedDataKeys = await AsyncStorage.getItem(dataKey);
 
-  function handleChangeInputText(text: string) {
-    // Update searchText value
-  }
+        const formatedStoragedDataKeys: LoginListDataProps =
+            storagedDataKeys && storagedDataKeys.length > 0
+                ? JSON.parse(storagedDataKeys)
+                : [];
 
-  useFocusEffect(useCallback(() => {
-    loadData();
-  }, []));
+        setSearchListData(formatedStoragedDataKeys);
+        setData(formatedStoragedDataKeys);
+    }
 
-  return (
-    <>
-      <Header
-        user={{
-          name: 'Rocketseat',
-          avatar_url: 'https://i.ibb.co/ZmFHZDM/rocketseat.jpg'
-        }}
-      />
-      <Container>
-        <SearchBar
-          placeholder="Qual senha você procura?"
-          onChangeText={handleChangeInputText}
-          value={searchText}
-          returnKeyType="search"
-          onSubmitEditing={handleFilterLoginData}
+    function handleFilterLoginData() {
+        // Filter results inside data, save with setSearchListData
+        if (searchListData.length > 0 && searchText !== "") {
+            const filteredText = searchListData.filter(
+                ({ service_name }) =>
+                    service_name
+                        .toLowerCase()
+                        .indexOf(searchText.toLowerCase()) > -1
+            );
 
-          onSearchButtonPress={handleFilterLoginData}
-        />
+            setSearchListData(filteredText);
+        } else {
+            setSearchListData(data);
+        }
+    }
 
-        <Metadata>
-          <Title>Suas senhas</Title>
-          <TotalPassCount>
-            {searchListData.length
-              ? `${`${searchListData.length}`.padStart(2, '0')} ao total`
-              : 'Nada a ser exibido'
-            }
-          </TotalPassCount>
-        </Metadata>
+    function handleChangeInputText(text: string) {
+        // Update searchText value
+        setSearchText(text);
+    }
 
-        <LoginList
-          keyExtractor={(item) => item.id}
-          data={searchListData}
-          renderItem={({ item: loginData }) => {
-            return <LoginDataItem
-              service_name={loginData.service_name}
-              email={loginData.email}
-              password={loginData.password}
+    useFocusEffect(
+        useCallback(() => {
+            loadData();
+        }, [])
+    );
+
+    return (
+        <>
+            <Header
+                user={{
+                    name: "Rocketseat",
+                    avatar_url: "https://i.ibb.co/ZmFHZDM/rocketseat.jpg",
+                }}
             />
-          }}
-        />
-      </Container>
-    </>
-  )
+            <Container>
+                <SearchBar
+                    placeholder="Qual senha você procura?"
+                    onChangeText={handleChangeInputText}
+                    value={searchText}
+                    returnKeyType="search"
+                    onSubmitEditing={handleFilterLoginData}
+                    onSearchButtonPress={handleFilterLoginData}
+                />
+
+                <Metadata>
+                    <Title>Suas senhas</Title>
+                    <TotalPassCount>
+                        {searchListData.length
+                            ? `${`${searchListData.length}`.padStart(
+                                  2,
+                                  "0"
+                              )} ao total`
+                            : "Nada a ser exibido"}
+                    </TotalPassCount>
+                </Metadata>
+
+                <LoginList
+                    keyExtractor={(item) => item.id}
+                    data={searchListData}
+                    renderItem={({ item: loginData }) => {
+                        return (
+                            <LoginDataItem
+                                service_name={loginData.service_name}
+                                email={loginData.email}
+                                password={loginData.password}
+                            />
+                        );
+                    }}
+                />
+            </Container>
+        </>
+    );
 }
